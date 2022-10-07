14b340ed8e58956384d73c660f4814af866a9001
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-10-07 14:24:48 +0300

    added context

---
+++ DapperAspNetCoreAPI/Context/DapperContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace DapperAspNetCoreAPI.Context
+{
+    public class DapperContext
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+
+        public DapperContext(IConfiguration configuration, string connectionString)
+        {
+            _configuration = configuration;
+            _connectionString = connectionString;   
+        }
+
+        public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
+    }
+}
--- DapperAspNetCoreAPI/Program.cs
+++ DapperAspNetCoreAPI/Program.cs
@@ -1,6 +1,10 @@
+using DapperAspNetCoreAPI.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddSingleton<DapperContext>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
