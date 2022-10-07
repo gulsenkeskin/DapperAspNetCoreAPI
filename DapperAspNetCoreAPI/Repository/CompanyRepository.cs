e48c10192fcb19e97f08147eb2665ec691fe713f
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-10-07 15:17:05 +0300

    Create CompanyRepository.cs

---
+++ DapperAspNetCoreAPI/Repository/CompanyRepository.cs
@@ -0,0 +1,27 @@
+﻿using Dapper;
+using DapperAspNetCoreAPI.Context;
+using DapperAspNetCoreAPI.Contracts;
+using DapperAspNetCoreAPI.Entities;
+
+namespace DapperAspNetCoreAPI.Repository
+{
+    public class CompanyRepository : ICompanyRepository
+    {
+        private readonly DapperContext _context;
+        public CompanyRepository(DapperContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Company>> GetCompanies()
+        {
+            var query = "SELECT * FROM Companies";
+            using (var connection = _context.CreateConnection())
+            {
+                var companies = await connection.QueryAsync<Company>(query);
+
+                return companies.ToList();
+            }
+        }
+    }
+}
